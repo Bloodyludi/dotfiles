8a07a941b9a5b23844bfae8449e9ed16ef07fe77
Ludwig Lauer <ludwig@tenjin.com>
2022-04-25 22:59:37 +0200

    Use LunarVim

--- setup.sh
+++ setup.sh
@@ -7,19 +7,17 @@ sudo -v
 if ! command -v brew >/dev/null 2>&1; then
   echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew analytics off
 fi
-
-# Disable analytics
-brew analytics off
 
 if ! command -v stow >/dev/null 2>&1; then
   echo "Installing stow..."
   brew install stow
 fi
 
-for DIR in {zsh,brew,vim,asdf}; do
-  echo "Symlinking $DIR..."
-  stow --verbose --ignore=\.DS_Store --target ~ "$DIR"
+for folder in {zsh,brew,lvim,asdf}; do
+  echo "Symlinking $folder..."
+  stow --verbose --ignore=\.DS_Store --target ~ "$folder"
 done
 
 echo "Installing applications via homebrew..."
@@ -34,9 +32,16 @@ cut -d ' ' -f1 ~/.tool-versions | xargs -I % asdf plugin add %
 echo "Installing asdf runtimes..."
 asdf install
 
+if ! command -v stow >/dev/null 2>&1; then
+  echo "Installing lunarVim..."
+  yarn_dir="$(yarn global bin 2>/dev/null)"
+  [[ -d $yarn_dir ]] || mkdir -p "$yarn_dir"
+  bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
+fi
+
 if ! test -e "${HOME}/.iterm2_shell_integration.zsh"; then
   echo "Installing iTerm2 shell integration..."
   curl -L https://iterm2.com/shell_integration/install_shell_integration.sh | /bin/bash
 fi
 
-./macos-defaults
+./macos-defaults.sh
